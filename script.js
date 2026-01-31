bbba00482923b370cdd04c77c3528354cb2b0432
DFDredd <122494362+DFDredd@users.noreply.github.com>
2026-01-31 13:24:14 -0700

    Fix product data and cart functionality

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const products = [
     image: "small-clamshell.jpg",
     colors: ["Black", "Blue", "Red"],
     description: "Small clamshells for seed storage/shipping"
-  },
+  }
   {
     id: 2,
     name: "Small BeanKan",
     price: 25.0,
     image: "small-beankan.jpg",
     description: "Small BeanKan has two layers and is used to germinate seeds without drowning them"
-  },
+  }
   {
     id: 3,
     name: "Poker Chip",
@@ -22,7 +22,7 @@ const products = [
     image: "poker-chip.jpg",
     colors: ["Black", "Blue", "Red", "Silver"],
     description: "Poker chips can be customizable with a simple logo or text for an additional fee"
-  },
+  }
   // Add more products here as needed
 ];
 
@@ -206,6 +206,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
 
 
 
